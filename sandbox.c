65e8b6c1194018848fc096943590e0462399ad50
Sergey Stankewich <127226614+stankstasgit23@users.noreply.github.com>
2023-04-26 14:01:01 +0300

    copy local repository
    copy local repository "sandbox1"

---
+++ sandbox.c
@@ -0,0 +1,13 @@
+/* main.c */
+#include <stdio.h>
+
+#include "askname.h"
+
+int main(int argc, char **argv)
+{	
+	char first[255], last[255];
+	askname(first, last);
+ 	printf("Hello, %s %s!\n", first, last);
+	return 0;
+}
+
